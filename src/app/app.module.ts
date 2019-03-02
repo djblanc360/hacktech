24376ac93021c52bd6affb7a39d6685a8ff6f795
djblanc360 <djblanc360@gmail.com>
2019-03-02 13:37:55 -0800

    updated angular and added ui library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,11 @@ import { AppComponent } from './app.component';
         import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 // services
 import {SpotifyService} from './spotify.service';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,9 +42,10 @@ import {SpotifyService} from './spotify.service';
     FormsModule,
     ReactiveFormsModule,
     NgxPaginationModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgZorroAntdModule
   ],
-  providers: [SpotifyService],
+  providers: [SpotifyService, { provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
